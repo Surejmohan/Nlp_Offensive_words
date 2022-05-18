69288ecfb75fffec7fa277005e07325954205092
Surejmohan <surej@almug.ai>
2022-05-19 01:16:36 +0530

    first commit

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import numpy as np
 from flask import Flask, jsonify, request
-from scipy.special import softmax
-from transformers import (AutoModelForSequenceClassification, AutoTokenizer)
+# from scipy.special import softmax
+# from transformers import (AutoModelForSequenceClassification, AutoTokenizer)
 
 app = Flask(__name__)
-MODEL = f"cardiffnlp/twitter-roberta-base-offensive"
-tokenizer = AutoTokenizer.from_pretrained(MODEL)
+# MODEL = f"cardiffnlp/twitter-roberta-base-offensive"
+# tokenizer = AutoTokenizer.from_pretrained(MODEL)
 
-model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+# model = AutoModelForSequenceClassification.from_pretrained(MODEL)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -17,16 +17,22 @@ def home():
     elif request.method == "POST":
         text = request.json.get("text")
 
-        encoded_input = tokenizer(text, return_tensors="pt")
-        output = model(**encoded_input)
-        scores = output[0][0].detach().numpy()
-        scores = softmax(scores)
-        ranking = np.argsort(scores)
+        # encoded_input = tokenizer(text, return_tensors="pt")
+        # output = model(**encoded_input)
+        # scores = output[0][0].detach().numpy()
+        # scores = softmax(scores)
+        # ranking = np.argsort(scores)
 
+        # return jsonify(
+        #     {
+        #         "not-offensive": str(scores[0]),
+        #         "offensive": str(scores[1]),
+        #     }
+        # )
         return jsonify(
             {
-                "not-offensive": str(scores[0]),
-                "offensive": str(scores[1]),
+                "not-offensive": 1,
+                "offensive": 1,
             }
         )
 
